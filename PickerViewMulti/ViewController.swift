44c76bdfab3d2c4044a6ae065e156be9956be5a1
soyoung <soyoung@geulin-akademiui-iMac.local>
2017-11-26 00:11:36 +0900

    Initial Commit

---
+++ PickerViewMulti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickerViewMulti
+//
+//  Created by soyoung on 2017. 11. 26..
+//  Copyright © 2017년 soyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
